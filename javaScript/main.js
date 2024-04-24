ae8fcea64b5ca0478c8c29d009e1c4820e3fd917
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-24 13:10:27 +0200

    milestone 3

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,12 +1,24 @@
 // recupera i dati che ti servono(img)
 let slides = document.getElementsByClassName("img-card");
-let attiva = document.getElementsByClassName("active");
-let nonAttiva = document.getElementsByClassName("none");
 let nSlides = slides.length;
+let slideSelected = 0;
+slides[slideSelected].classList.add("active")
+// devo far cambiare l'immagine al click del bottone (next)
+document.getElementById("next").addEventListener("click", function () {
+    slides[slideSelected].classList.remove("active");
+    if (slideSelected < nSlides - 1){
+        slideSelected++;
+    }
+    else {
+        slideSelected = 0;
+    }
+    slides[slideSelected].classList.add("active")
+})
 
-// devo far cambiare l'immagine
-slides[0].classList.add("active")
-if (slides[0].classList.contains("active")) {
-    slides[0].classList.remove("active")
-    slides[1].classList.add("active")
-}
+
+
+// slides[0].classList.add("active")
+// if (slides[0].classList.contains("active")) {
+//     slides[0].classList.remove("active")
+//     slides[1].classList.add("active")
+// }
