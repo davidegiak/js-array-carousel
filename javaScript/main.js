ef6ac211fe12527f126823cae68def0bf368ce44
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-24 11:26:23 +0200

    non ci riesco

---
+++ javaScript/main.js
@@ -0,0 +1,12 @@
+// recupera i dati che ti servono(img)
+let slides = document.getElementsByClassName("img-card");
+let attiva = document.getElementsByClassName("active");
+let nonAttiva = document.getElementsByClassName("none");
+let nSlides = slides.length;
+
+let slideSelected = 1;
+
+// devo far cambiare l'immagine
+    
+attiva.classList.remove("active")
+
